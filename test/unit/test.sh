d3a325f22f3531613ba845345d9545f2a60414ca
toughHaile <hailemariam.dd0909@gmail.com>
2025-06-20 08:40:12 +0300

    Update test.sh

--- test/unit/test.sh
+++ test/unit/test.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
+# Initialize
+./minigit init
 
+# Test basic workflow
+echo "Hello" > file.txt
+./minigit add file.txt
+./minigit commit -m "First commit"
+
+# Test branching
+./minigit branch dev
+./minigit checkout dev
+echo "World" >> file.txt
+./minigit commit -m "Dev commit"
+
+# Test merge
+./minigit checkout main
+./minigit merge dev  # Should show conflict if file.txt changed in both
+
+# View log
+./minigit log
