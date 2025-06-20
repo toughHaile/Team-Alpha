4a4bcd96a1985c7dd78c95128cbd58fbaff4986d
houtfd <hiruyedereje1@gmail.com>
2025-06-20 11:41:45 +0300

    Update StagingArea.hpp

--- include/StagingArea.hpp
+++ include/StagingArea.hpp
@@ -1,1 +1,26 @@
+ #pragma once 
+#include <unordered_map> 
+#include <string> 
  
+using namespace std; 
+ 
+class StagingArea { 
+private: 
+    unordered_map<string, string> stagedFiles; // filename -> blobHash 
+ 
+public: 
+    void stage(const string& filename, const string& hash) { 
+        // Unique: Prevent duplicate staging 
+        if (stagedFiles.find(filename) != stagedFiles.end()) 
+            throw runtime_error("File already staged: " + filename); 
+        stagedFiles[filename] = hash; 
+    } 
+ 
+    const unordered_map<string, string>& getStagedFiles() const { 
+        return stagedFiles; 
+    } 
+ 
+    void clear() { 
+        stagedFiles.clear(); 
+    } 
+};
