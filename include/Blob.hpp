6b0e49259f569fdb5335cffdf7320fb8ad0bf6f6
HearmonT <hearmon.ugr-4747-16@aau.edu.et>
2025-06-20 07:03:00 +0300

    Update Blob.hpp
    Blob Class
    
    The Blob class handles content hashing and retrieval for a simple version control system.
    
    hash(content): Generates a unique SHA-1 hash for the given content using a timestamp-based salt.
    
    load(hash): Loads the content of a stored object from .minigit/objects/ using its hash.

--- include/Blob.hpp
+++ include/Blob.hpp
@@ -1,1 +1,23 @@
- 
+#pragma once 
+#include <string>
+#include <openssl/sha.h>
+#include <chrono>
+using namespace std;
+class Blob { 
+public: 
+static string hash(const string& content){  
+ auto salt = to_string(chrono::system_clock::now().time_since_epoch().count());  
+ string salted = content + salt;           
+ unsigned char digest[SHA_DIGEST_LENGTH];    
+ SHA1((const unsigned char*)salted.c_str(), salted.size(), digest);    
+ char hex[SHA_DIGEST_LENGTH*2+1];   
+ for (int i = 0; i < SHA_DIGEST_LENGTH; i++)     
+  sprintf(hex + i*2, "%02x", digest[i]);       
+ return string(hex);   
+}    
+static string load(const string& hash) {    
+ ifstream file(".minigit/objects/" + hash); 
+ return string((istreambuf_iterator<char>(file)),      
+  istreambuf_iterator<char>());
+}
+};
